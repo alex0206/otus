6cc13e680ff54e82af665670ddaab4398d8066af
Alexey Epishev <alexeyepishev@ya.ru>
2020-03-08 09:17:11 +0400

    The first lesson is finished

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"os"
+	"time"
+)
+
 func main() {
-	// Place your code here
+	printLocalTime()
+	printNtpTime()
+}
+
+func printLocalTime() {
+	fmt.Println("current time:", time.Now())
+}
+
+func printNtpTime() {
+	ntpTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println("exact time:", ntpTime)
 }
