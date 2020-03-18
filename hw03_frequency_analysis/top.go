dd2141bd02a48ece27185711895c536847f51b32
Alexey Epishev <alexeyepishev@ya.ru>
2020-03-18 21:23:49 +0400

    Added base code

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,23 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
 
-func Top10(_ string) []string {
-	// Place your code here
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"strings"
+)
+
+func Top10(text string) []string {
+	scanner := bufio.NewScanner(strings.NewReader(text))
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalln(err.Error())
+	}
+
 	return nil
 }
