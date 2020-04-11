f45c606fb41318af52ec37fb8ed6adf51dc24f34
Alexey Epishev <alexeyepishev@ya.ru>
2020-04-11 23:55:46 +0400

    Added linked list implementation

--- hw04_lru_cache/go.mod
+++ hw04_lru_cache/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw04_lru_cache
+module github.com/alex0206/otus/hw04_lru_cache
 
 go 1.14
 
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,15 +1,80 @@
 package hw04_lru_cache //nolint:golint,stylecheck
 
 type List interface {
-	// Place your code here
+	Len() int
+	Front() *listItem
+	Back() *listItem
+	PushFront(v interface{}) *listItem
+	PushBack(v interface{}) *listItem
+	Remove(i *listItem)
+	MoveToFront(i *listItem)
 }
 
 type listItem struct {
-	// Place your code here
+	Value interface{}
+	Next  *listItem
+	Prev  *listItem
 }
 
 type list struct {
-	// Place your code here
+	front *listItem
+	back  *listItem
+	cnt   int
+}
+
+func (l list) Len() int {
+	return l.cnt
+}
+
+func (l list) Front() *listItem {
+	return l.front
+}
+
+func (l list) Back() *listItem {
+	return l.back
+}
+
+func (l list) PushFront(v interface{}) *listItem {
+	l.front = &listItem{
+		Value: v,
+		Next:  l.Front(),
+		Prev:  nil,
+	}
+	l.cnt++
+
+	return l.front
+}
+
+func (l list) PushBack(v interface{}) *listItem {
+	l.back = &listItem{
+		Value: v,
+		Next:  nil,
+		Prev:  l.Back(),
+	}
+	l.cnt++
+
+	return l.back
+}
+
+func (l list) Remove(i *listItem) {
+	i.Prev.Next = i.Next
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+	i = nil
+	l.cnt--
+}
+
+func (l list) MoveToFront(i *listItem) {
+	i.Prev.Next = i.Next
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+
+	l.front.Prev = i
+	i.Prev = nil
+	i.Next = l.front
+	l.front = i
 }
 
 func NewList() List {
